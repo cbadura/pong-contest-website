3e71b3a16798b18882b56458c38d6e150ad5d8a2
Christian Badura <cbadura@c4c1c5.42berlin.de>
2023-09-25 18:03:37 +0200

    first game prototype

--- src/app/game/square.ts
+++ src/app/game/square.ts
@@ -1,15 +1,23 @@
 export class Square {
-  constructor(private ctx: CanvasRenderingContext2D, private sizeX: number,
-    private sizeY: number) {}
+  constructor(private ctx: CanvasRenderingContext2D,
+    public x: number, public y: number,
+    private sizeX: number, private sizeY: number) {}
 
-  draw(x: number, y: number) {
-    this.ctx.fillRect(x, y, this.sizeX, this.sizeY);
+  draw() {
+    this.ctx.fillRect(this.x, this.y, this.sizeX, this.sizeY);
   }
 
-  move() {
-    const max = this.ctx.canvas.height - 100;
-    const canvas = this.ctx.canvas;
-    let y = 0;
+  moveBy(distance: number) {    
+    this.y += distance;
+  }
+}
+
+/* GAME LOOP:
+To stop a timer created by setInterval, you need to call
+clearInterval and give it the identifier for the interval you
+want to cancel. The id to use is the one that is
+returned by setInterval, and this is why we need to store it.
+let y = 0;
     const itval = setInterval(() => {
       this.ctx.clearRect(0, 0, canvas.width, canvas.height);
       this.draw(10, 20 + y);
@@ -17,11 +25,4 @@ export class Square {
       if (y >= max) {
         clearInterval(itval);
       }
-    }, 200);
-  }
-}
-
-/* To stop a timer created by setInterval, you need to call
-clearInterval and give it the identifier for the interval you
-want to cancel. The id to use is the one that is
-returned by setInterval, and this is why we need to store it. */
+    }, 200);*/
